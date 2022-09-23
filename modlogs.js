7d76e1128297afc08025bfb9f1eb6f3c049e453a
LunarCodes <91988772+LunarTaku@users.noreply.github.com>
2022-09-23 20:51:13 +0300

    made the command look better

--- modlogs.js
+++ modlogs.js
@@ -50,101 +50,116 @@ module.exports = {
    */
   async execute(interaction) {
     const getSubCommand = interaction.options.getSubcommand();
-    if (getSubCommand === "setup") {
-      const { options } = interaction;
-      const channel = options.getChannel("channel");
-      const modSys = await modSchema.findOne({
-        guildId: interaction.guild.id,
-      })
-  
-      if(modSys === interaction.guild.id) {
-        interaction.reply({
-          content: "Modlogs are already setup!",
-        })
-        return;
-      }
 
-      const newModlogs = new modSchema({
-        _id: mongoose.Types.ObjectId(),
-        guildId: interaction.guild.id,
-        channelId: channel.id,
-      });
+    switch (interaction.options.getSubcommand()) {
+      case "setup":
+        {
+          const { options } = interaction;
+          const channel = options.getChannel("channel");
+          const modSys = await modSchema.findOne({
+            guildId: interaction.guild.id,
+          });
 
-      newModlogs.save().catch((err) => console.log(err));
-      await interaction
-        .reply({
-          embeds: [
-            new EmbedBuilder()
-              .setTitle("Modlogs setup!")
-              .setDescription(
-                `Modlogs have been successfully setup in ${channel.mention}`
-              )
-              .setColor(0x00ff00),
-          ],
-          ephemeral: true,
-        })
-        .catch((err) => console.log(err));
-    } else if (getSubCommand === "replace_channel") {
-      const { options } = interaction;
-      const channel = options.getChannel("channel");
-      const modlogs = await modSchema.findOne({
-        guildId: interaction.guild.id,
-      });
+          if (modSys === interaction.guild.id) {
+            interaction.reply({
+              content: "Modlogs are already setup!",
+            });
+            return;
+          }
 
-      if (!modlogs) {
-        return interaction.reply({
-          content: "Modlogs not setup! To setup run `/modlogs setup`",
+          const newModlogs = new modSchema({
+            _id: mongoose.Types.ObjectId(),
+            guildId: interaction.guild.id,
+            channelId: channel.id,
+          });
+
+          newModlogs.save().catch((err) => console.log(err));
+          await interaction
+            .reply({
+              embeds: [
+                new EmbedBuilder()
+                  .setTitle("Modlogs setup!")
+                  .setDescription(
+                    `Modlogs have been successfully setup in ${channel.mention}`
+                  )
+                  .setColor(0x00ff00),
+              ],
+              ephemeral: true,
+            })
+            .catch((err) => console.log(err));
+        }
+
+        break;
+
+      case "replace_channel":
+        {
+          const { options } = interaction;
+          const channel = options.getChannel("channel");
+          const modlogs = await modSchema.findOne({
+            guildId: interaction.guild.id,
+          });
+
+          if (!modlogs) {
+            return interaction.reply({
+              content: "Modlogs not setup! To setup run `/modlogs setup`",
+            });
+          }
+
+          modSchema
+            .findOneAndUpdate(
+              { guildId: interaction.guild.id },
+              { channelId: channel.id }
+            )
+            .catch((err) => console.log(err));
+
+          await interaction
+            .reply({
+              embeds: [
+                new EmbedBuilder()
+                  .setTitle("Modlogs channel replaced!")
+                  .setDescription(
+                    `Modlogs channel has been successfully replaced in <#${channel.id}>`
+                  )
+                  .setColor(0x00ff00),
+              ],
+              ephemeral: true,
+            })
+            .catch((err) => console.log(err));
+        }
+
+        break;
+
+      case "delete": {
+        const modlogs = await modSchema.findOne({
+          guildId: interaction.guild.id,
         });
+
+        if (!modlogs) {
+          return interaction.reply({
+            content: "Modlogs not setup! To setup run `/modlogs setup`",
+          });
+        }
+
+        modSchema
+          .findOneAndDelete({
+            guildId: interaction.guild.id,
+          })
+          .catch((err) => console.log(err));
+
+        await interaction
+          .reply({
+            embeds: [
+              new EmbedBuilder()
+                .setTitle("Modlogs deleted!")
+                .setDescription(`Modlogs have been successfully deleted!`)
+                .setColor(0x00ff00),
+            ],
+            ephemeral: true,
+          })
+          .catch((err) => console.log(err));
       }
-
-      modSchema
-        .findOneAndUpdate(
-          { guildId: interaction.guild.id },
-          { channelId: channel.id }
-        )
-        .catch((err) => console.log(err));
-
-      await interaction
-        .reply({
-          embeds: [
-            new EmbedBuilder()
-              .setTitle("Modlogs channel replaced!")
-              .setDescription(
-                `Modlogs channel has been successfully replaced in <#${channel.id}>`
-              )
-              .setColor(0x00ff00),
-          ],
-          ephemeral: true,
-        })
-        .catch((err) => console.log(err));
-    } else if(getSubCommand === "delete") {
-      const modlogs = await modSchema.findOne({
-        guildId: interaction.guild.id,
-      });
-
-      if (!modlogs) {
-        return interaction.reply({
-          content: "Modlogs not setup! To setup run `/modlogs setup`",
-        });
-      }
-
-      modSchema.findOneAndDelete({
-        guildId: interaction.guild.id,
-      }).catch((err) => console.log(err));
-
-      await interaction
-        .reply({
-          embeds: [
-            new EmbedBuilder()
-              .setTitle("Modlogs deleted!")
-              .setDescription(
-                `Modlogs have been successfully deleted!`
-              )
-              .setColor(0x00ff00),
-          ],
-          ephemeral: true,
-        })
-        .catch((err) => console.log(err));
+      default:
+        break;
     }
   },
 };
